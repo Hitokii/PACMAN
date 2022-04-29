6caad440d76adeb02b3e53b1e6064ca6eb22180b
Hitoki <phitoki@outlook.fr>
2022-04-29 18:34:23 +0200

    BIG UPDATE
    TODO: ghost more coint
    multiplayer ?

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from tkinter import ON
 import pygame;
-
-
                                                        
 #  ____   ____  _       _______     _____       _       ______   _____     ________   ______   
 # |_  _| |_  _|/ \     |_   __ \   |_   _|     / \     |_   _ \ |_   _|   |_   __  |.' ____ \  
@@ -17,6 +16,7 @@ UP = False;
 DOWN = False;
 
 
+
 #DEFINITION D'ECRAN
 w,h = 900,900;
 SCREEN = pygame.display.set_mode((w,h));
@@ -29,36 +29,38 @@ BG = pygame.transform.scale(BG,(w,h))
 BG_hitbox = BG.get_rect();
 
 
-#PACMAN
-player = pygame.image.load("asset/2.png");
+
+#PACMANANIMATED
+frame = 0
+player = pygame.image.load("asset/2.png")
 player = pygame.transform.scale(player,(40,40))
 player_hitbox = player.get_rect();
-player_hitbox.center = w//3,340
+player_hitbox.center = w//2,340
 
 
 
 
 
+pacgumarray = [
+    pygame.image.load("asset/pg.png"),
+    pygame.image.load("asset/pg.png"),
+    pygame.image.load("asset/pg.png"),
+    pygame.image.load("asset/pg.png"),
+    pygame.image.load("asset/pg.png"),
+    pygame.image.load("asset/pg.png"),
+    pygame.image.load("asset/pg.png"),
+    pygame.image.load("asset/pg.png"),
+    pygame.image.load("asset/pg.png"),
+    pygame.image.load("asset/pg.png"),
+    pygame.image.load("asset/pg.png"),
+    pygame.image.load("asset/pg.png")
+]
 
-#MURS 18
-#collider = [
-#    #MUR PRINCIPAUX
-#    pygame.Rect(0,0,900,35),
-#    pygame.Rect(0,0,35,395),
-#    pygame.Rect(0,460,35,500),
-#    pygame.Rect(870,0,35,395),
-#    pygame.Rect(870,460,35,500),
-#    pygame.Rect(0,870,900,35),
-#    pygame.Rect(0,870,900,35),
-#    #PLATFORM
-#    pygame.Rect(90,84,97,55),
-#    pygame.Rect(250,84,120,60),
-#    pygame.Rect(95,195,93,30),
-#    pygame.Rect(720,195,93,30),
-#    ]
+ON_COIN = False
+POINT = 0
+pacgumHit = []
 
-empty_square = pygame.Rect(-50,-50,1,1)
-current_collision = empty_square
+distance = 30
 
 #     ______      _      ____    ____ ________  
 #   .' ___  |    / \    |_   \  /   _|_   __  | 
@@ -68,9 +70,18 @@ current_collision = empty_square
 #   `._____.|____| |____|_____||_____|________| 
 #                                                                                      
 
-while (True):
-    
+while (True):    
     MOUSEX,MOUSEY = pygame.mouse.get_pos()
+
+    frame +=1
+    if frame <= 50:
+        player = pygame.image.load("asset/2.png");
+        player = pygame.transform.scale(player,(40,40))
+    if frame >50:
+        player = pygame.image.load("asset/1.png")
+        player = pygame.transform.scale(player,(40,40))
+    if frame == 100:
+        frame = 0
 
 
 #                                                                  _         
@@ -83,22 +94,34 @@ while (True):
     
     if (UP):
         player_hitbox.y -= 1
-        player = pygame.image.load("asset/2.png");
+        if frame <= 30:
+            player = pygame.image.load("asset/2.png");
+        if frame >30:
+            player = pygame.image.load("asset/1.png")
         player = pygame.transform.scale(player,(40,40))
         player = pygame.transform.rotate(player,90)
     if (DOWN):
         player_hitbox.y += 1
-        player = pygame.image.load("asset/2.png");
+        if frame <= 30:
+            player = pygame.image.load("asset/2.png");
+        if frame >30:
+            player = pygame.image.load("asset/1.png")
         player = pygame.transform.scale(player,(40,40))
         player = pygame.transform.rotate(player,-90)
     if (LEFT):
         player_hitbox.x -= 1
-        player = pygame.image.load("asset/2.png");
+        if frame <= 30:
+            player = pygame.image.load("asset/2.png");
+        if frame >30:
+            player = pygame.image.load("asset/1.png")
         player = pygame.transform.scale(player,(40,40))
         player = pygame.transform.rotate(player,180)
     if (RIGHT):
         player_hitbox.x += 1
-        player = pygame.image.load("asset/2.png");
+        if frame <= 30:
+            player = pygame.image.load("asset/2.png");
+        if frame >30:
+            player = pygame.image.load("asset/1.png")
         player = pygame.transform.scale(player,(40,40))
 
 #                        _               __         
@@ -135,6 +158,8 @@ while (True):
                 LEFT = False
                 UP = False
                 DOWN = False
+            if event.key == pygame.K_SPACE:
+                pygame.quit()
 
 #               __  __   _         _                       
 #              [  |[  | (_)       (_)                      
@@ -165,22 +190,45 @@ while (True):
         player_hitbox.y -= 1
         BOTTOM = False
 
+    colorr,colorg,colorb,colora = pygame.Surface.get_at(SCREEN,(player_hitbox.centerx,player_hitbox.centery))
+    if colorr == 237:
+        ON_COIN = True
+    else:
+        ON_COIN = False
 
 
 
-
-    
     SCREEN.blit(BG, BG_hitbox)
     SCREEN.blit(player, player_hitbox)
+    
+    for each in range(len(pacgumarray)):
+        if (each <=4):
+            pacgumHit.append(pacgumarray[each].get_rect())
+            pacgumHit[each].center = w//2.9+(each*70),h//2.65
+            SCREEN.blit(pacgumarray[each],pacgumHit[each])
+        if (each > 4 and each <10):
+            pacgumHit.append(pacgumarray[each].get_rect())
+            pacgumHit[each].center = w//2.9+((each-5)*70),h//1.78
+            SCREEN.blit(pacgumarray[each],pacgumHit[each])
+        if (each == 10):
+            pacgumHit.append(pacgumarray[each].get_rect())
+            pacgumHit[each].center = w//2.9,h//2.15
+            SCREEN.blit(pacgumarray[each],pacgumHit[each])
+        if (each == 11):
+            pacgumHit.append(pacgumarray[each].get_rect())
+            pacgumHit[each].center = w//1.52,h//2.15
+            SCREEN.blit(pacgumarray[each],pacgumHit[each])
 
-#    if (HIT):
-#        pygame.draw.rect(player,(255,255,255),(0,0,50,50))
-#        for each in range(len(collider)):
-#            pygame.draw.rect(SCREEN,(255,0,0),collider[each])
+    if ((POINT//2) == len(pacgumarray)):
+        pygame.quit()
+    
+    if (ON_COIN):
+        POINT += 1
 
+    #PACGUM
+    for each in range(len(pacgumarray)):
+        if (player_hitbox.colliderect(pacgumHit[each]) and ON_COIN == True ):
+            ON_COIN = False
+            pacgumarray[each].set_alpha(0)
 
     pygame.display.update()
-
-
-
-
---
+++ paclass.py
@@ -0,0 +1,7 @@
+import pygame
+
+
+class pacgum:
+    def __init__(self) -> None:
+        pygame.image.load("asset/pg.png");
+        
